5bab2414fcfebb59ddcc78c571d9e4bde8fde249
diaspd <pdr050203@gmail.com>
2023-11-11 11:46:59 -0300

    added opengraph image to app

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,67 @@
+import { ImageResponse } from 'next/server'
+import Image from 'next/image'
+
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { env } from '@/env'
+import colors from 'tailwindcss/colors'
+
+export const runtime = 'edge'
+
+export const alt = 'About Acme'
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function OgImage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const product = await getProduct(params.slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: colors.zinc[950],
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img
+          src={productImageURL}
+          style={{
+            width: '100%',
+            border: '4px solid',
+            borderRadius: '10px',
+            borderColor: colors.zinc[100],
+          }}
+          alt=""
+        />
+      </div>
+    ),
+    {
+      ...size,
+    },
+  )
+}
