f240386b51deb097aaab8e9abea0ad4ee8fca098
Pedro Dias <pdr050203@gmail.com>
2023-11-18 12:42:39 -0300

    add: some style to focus on main input

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -26,19 +26,21 @@ export function SearchForm() {
   }
 
   return (
-    <form
-      onSubmit={handleSearch}
-      className="flex max-w-xs lg:max-w-none lg:w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-700"
-    >
-      <Search className="w-5 h-5 text-zinc-500" />
+    <div className="max-w-xs lg:max-w-none lg:w-[320px] items-center rounded-full bg-zinc-900 px-5 py-3 ring-700 focus-within:ring-2 focus-within:ring-zinc-400">
+      <form onSubmit={handleSearch} className="flex gap-3 border-none">
+        <label htmlFor="query">
+          <Search className="w-5 h-5 text-zinc-500" />
+        </label>
 
-      <input
-        name="q"
-        defaultValue={query ?? ''}
-        placeholder="Buscar produtos..."
-        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-        required
-      />
-    </form>
+        <input
+          name="q"
+          id="query"
+          defaultValue={query ?? ''}
+          placeholder="Buscar produtos..."
+          className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500 rounded"
+          required
+        />
+      </form>
+    </div>
   )
 }
