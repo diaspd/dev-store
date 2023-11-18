f4cbd478dfd6b6c16344648a1f81796a2b20e735
Pedro Dias <pdr050203@gmail.com>
2023-11-18 12:03:39 -0300

    add: searchform

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
-import { Search } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export function Header() {
   return (
@@ -11,14 +11,7 @@ export function Header() {
           devstore
         </Link>
 
-        <form className="flex max-w-xs lg:max-w-none lg:w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
 
       <div className="flex self-start lg:self-auto items-center gap-4">
