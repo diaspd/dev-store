e4617c2be1629a4565c5889ea6c0bc4b133b057a
Pedro Dias <pdr050203@gmail.com>
2023-11-20 22:09:40 -0300

    add: tests for add product to cart page

--- cypress/e2e/hello-world.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://rocketseat.com.br')
-
-    cy.contains('Quero aproveitar', {
-      matchCase: false,
-    }).click()
-  })
-})
