ed49b199f056627a61b3f638a01e1abd8483dd8a
Pedro Dias <pdr050203@gmail.com>
2023-11-20 22:31:54 -0300

    add: test for search page

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,9 @@
 describe('add product to cart', () => {
-  it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
+  it('should be able to navigate to the product page and add it to the cart', () => {
     cy.get('a[href^="/product"]').first().click()
 
     cy.url().should('include', '/product')
@@ -13,8 +15,6 @@ describe('add product to cart', () => {
   })
 
   it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('a[href^="/product"]').first().click()
 
     cy.location('pathname').should('include', '/product')
@@ -26,8 +26,6 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('input[name=q]').type('moletom').parent('form').submit()
 
     cy.get('a[href^="/product"]').first().click()
